ea8e11dcc85c975037a1d9626bc7031a218c5438
wardones <85134965+wardones@users.noreply.github.com>
2022-04-07 19:55:14 -0400

    Create buyrev.lua

---
+++ buyrev.lua
@@ -0,0 +1,9 @@
+        local Plr = game.Players.LocalPlayer
+        local OldPos = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position
+        wait(0.3)
+        Plr.Character.HumanoidRootPart.CFrame = game.Workspace.Ignored.Shop["[Revolver] - $1300"].Head.CFrame
+        wait(0.35)
+        fireclickdetector(game.Workspace.Ignored.Shop["[Revolver] - $1300"].ClickDetector)
+        wait(0.4)
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(OldPos)
+        wait(0.25)
